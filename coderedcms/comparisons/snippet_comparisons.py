5d1ab9cb60036654180672f2e2c9a343a06821fc
Jayden Kneller <jayden@arrai.com>
2024-02-05 15:44:05 -0700

    Changes to deal with data that doesn't have revisions yet.

--- coderedcms/comparisons/snippet_comparisons.py
+++ coderedcms/comparisons/snippet_comparisons.py
@@ -1,4 +1,4 @@
-from django.shortcuts import get_object_or_404
+from django.core.exceptions import ObjectDoesNotExist
 from django.utils.html import format_html
 from django.utils.html import format_html_join
 
@@ -8,14 +8,106 @@ from wagtail.admin.compare import (
 )
 
 from wagtail.admin.panels import get_edit_handler
+from wagtail.snippets.blocks import SnippetChooserBlock
 
 
 class ReusableContentComparison(BlockComparison):
     def get_revision(self, val):
         instance = val["content"]
         revision_id = val["revision"]
-        revision = get_object_or_404(instance.revisions, id=revision_id)
+        if revision_id is None:
+            try:
+                revision = instance.revisions.latest("pk")
+            except ObjectDoesNotExist:
+                # There are no revisions set up for this reusable content yet.
+                return None
+            revision_id = revision.pk
+        try:
+            revision = instance.revisions.get(id=revision_id)
+        except ObjectDoesNotExist:
+            # There are no revisions set up for this reusable content yet.
+            return None
         return revision.as_object()
+
+    def _get_revisions_diff(self):
+        instance = self.val_a["content"]
+        revision_a = self.get_revision(self.val_a)
+        revision_b = self.get_revision(self.val_b)
+
+        # Copied from with modifications:
+        # https://github.com/wagtail/wagtail/blob/
+        # 9edaadaef094d7732025121c32313472b353ace8/
+        # wagtail/admin/views/generic/models.py#L739-L752
+        comparison = (
+            get_edit_handler(self.val_a["content"])
+            .get_bound_panel(instance=instance)
+            .get_comparison()
+        )
+
+        htmldiffs = []
+        for comp in comparison:
+            # If there are no revisions set up for a reusable content,then
+            # diff against the instance, so we can see the current data.
+            if revision_a is None:
+                revision_a = instance
+
+            if revision_b is None:
+                revision_b = instance
+
+            diff = comp(revision_a, revision_b)
+            htmldiffs.append(
+                (
+                    diff.field_label(),
+                    diff.htmldiff(),
+                )
+            )
+
+        return (
+            # Use "Reusable Content" instead or "Content",
+            # for a better looking diff.
+            instance._meta.verbose_name,
+            format_html(
+                "<dl>\n{}\n</dl>",
+                format_html_join(
+                    "\n",
+                    "    <dt>{}</dt>\n    <dd>{}</dd>",
+                    htmldiffs,
+                ),
+            ),
+        )
+
+    def htmlvalue(self, val):
+        htmlvalues = []
+        for name, block in self.block.child_blocks.items():
+            label = self.block.child_blocks[name].label
+
+            if name == "revision":
+                # Ignore this field, we don't want to display diffs for it.
+                continue
+
+            elif name == "content" and isinstance(block, SnippetChooserBlock):
+                # Override the diff to display the diff of the revision's
+                # content you are comparing, not the pk of the snippet.
+                revisions_diff = self._get_revisions_diff()
+                htmlvalues.append(revisions_diff)
+
+            else:
+                comparison_class = get_comparison_class_for_block(block)
+
+                htmlvalues.append(
+                    (
+                        label,
+                        comparison_class(
+                            block, True, True, val[name], val[name]
+                        ).htmlvalue(val[name]),
+                    )
+                )
+        return format_html(
+            "<dl>\n{}\n</dl>",
+            format_html_join(
+                "\n", "    <dt>{}</dt>\n    <dd>{}</dd>", htmlvalues
+            ),
+        )
 
     def htmldiff(self):
         htmldiffs = []
@@ -27,29 +119,9 @@ class ReusableContentComparison(BlockComparison):
                 continue
 
             elif name == "content":
-                instance = self.val_a["content"]
-                revision_a = self.get_revision(self.val_a)
-                revision_b = self.get_revision(self.val_b)
-
-                # Copied from with modifications:
-                # https://github.com/wagtail/wagtail/blob/
-                # 9edaadaef094d7732025121c32313472b353ace8/
-                # wagtail/admin/views/generic/models.py#L739-L752
-                comparison = (
-                    get_edit_handler(self.val_a["content"])
-                    .get_bound_panel(instance=instance)
-                    .get_comparison()
-                )
-
-                for comp in comparison:
-                    diff = comp(revision_a, revision_b)
-                    if diff.has_changed():
-                        htmldiffs.append(
-                            (
-                                instance._meta.verbose_name,
-                                diff.htmldiff(),
-                            )
-                        )
+                # Override the diff to display the diff of the revision's
+                # content you are comparing, not the pk of the snippet.
+                htmldiffs.append(self._get_revisions_diff())
 
             else:
                 comparison_class = get_comparison_class_for_block(block)
