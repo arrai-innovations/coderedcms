30d0bd457c657d23aab5d6390dad4bca3713fd82
Jayden Kneller <jayden@arrai.com>
2026-02-10 14:25:53 -0700

    chore: update the version number

--- coderedcms/__init__.py
+++ coderedcms/__init__.py
@@ -15,7 +15,7 @@ Maintains version of coderedcms.
 See: https://www.python.org/dev/peps/pep-0440/
 """
 
-release = ["6", "0", "3", "", ""]
+release = ["6", "0", "4", "", ""]
 
 
 def _get_version() -> str:
